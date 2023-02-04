abdb56f9cb40427ddb3cacaf2ebe27e41f050519
HanhDinh1 <ms.myhanh89@gmail.com>
2023-02-03 19:52:10 -0500

    edit cors in index.js

--- index.js
+++ index.js
@@ -4,7 +4,15 @@ const express = require("express");
 const cors = require("cors");
 
 const app = express();
-app.use(cors());
+// app.use(cors());
+
+app.use(
+    cors({
+      credentials: true,
+      // origin: '*',
+      origin: process.env.ORIGIN,
+    })
+  );
 
 app.get("/", (req, res) => {
     res.download("./HanhResume.png");
